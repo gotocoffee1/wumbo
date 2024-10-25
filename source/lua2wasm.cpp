3ef6fe81d224366ff5abb19c55d3dec438bd549d
gotocoffee <gotocoffee1@gmail.com>
2024-10-25 23:53:04 +0200

    cleanup the build

--- source/lua2wasm.cpp
+++ source/lua2wasm.cpp
@@ -12,14 +12,21 @@ wasm::mod compile(const block& chunk)
     c.build_types();
     c.convert(chunk); // remove
 
-
     //c.convert();
     //BinaryenSetStart(mod, v);
     //BinaryenAddFunctionExport(mod, "convert", "start");
     //BinaryenModuleAutoDrop(mod);
+
     BinaryenModuleValidate(mod);
-    //BinaryenModuleInterpret(mod);
-    //BinaryenModuleOptimize(mod);
+
+    auto feature = BinaryenModuleGetFeatures(mod);
+    auto res = BinaryenModuleAllocateAndWrite(mod, nullptr);
+    result.impl.reset(BinaryenModuleReadWithFeatures((char*)res.binary, res.binaryBytes, feature));
+    mod = reinterpret_cast<BinaryenModuleRef>(result.impl.get());
+    free(res.binary);
+    free(res.sourceMap);
+    BinaryenModuleOptimize(mod);
+
     return result;
 }
 
