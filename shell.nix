6358feaeda4ddd6137c110e73489891c669c38ce
gotocoffee <gotocoffee1@gmail.com>
2025-02-09 16:17:11 +0100

    add more tools to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,15 +3,24 @@
 }:
 
 pkgs.mkShell {
-  packages = with pkgs; [
-    cmake
-    ninja
-    ccache
-    gcc
-    clang-tools
-    vcpkg
-    pkg-config
-  ];
+  packages =
+    with pkgs;
+    [
+      cmake
+      ninja
+    ]
+    ++ [
+      ccache
+      clang-tools
+    ]
+    ++ [
+      vcpkg
+      pkg-config
+    ]
+    ++ [
+      gcc
+      emscripten
+    ];
   shellHook = ''
     export VCPKG_ROOT=${pkgs.vcpkg}/share/vcpkg
   '';
