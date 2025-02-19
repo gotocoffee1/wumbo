4051b06cd22e06a4985807898071e0c6875c8d8b
gotocoffee <gotocoffee1@gmail.com>
2025-02-20 00:21:01 +0100

    added testbox in html

--- js/load.mjs
+++ js/load.mjs
@@ -1,57 +1,58 @@
 import wumbo from "./wumbo.mjs";
 
-export const newInstance = async(override)=>{
+export const newInstance = async (override) => {
+  const instantiateBuffer = async (buffer) => {
+    const { module, instance } = await WebAssembly.instantiate(
+      buffer,
+      importObject,
+    );
+    const { start } = instance.exports;
+    return start;
+  };
 
-const instantiateBuffer = async (buffer) => {
-  const { module, instance } = await WebAssembly.instantiate(
-    buffer,
-    importObject,
-  );
-  const { start } = instance.exports;
-  return start;
-};
-
-const importObject = {
-  print: {
-    value: (arg) => {
-      console.log(arg);
+  const importObject = {
+    print: {
+      value: (arg) => {
+        console.log(arg);
+      },
+      string: (arg) => {
+        arg = new TextDecoder().decode(arg);
+        console.log(arg);
+      },
+      array: (size) => new Uint8Array(size),
+      set_array: (array, index, value) => (array[index] = value),
     },
-    string: (arg) => {
-      arg = new TextDecoder().decode(arg);
-      console.log(arg);
+    load: {
+      load: instantiateBuffer,
     },
-    array: (size) => new Uint8Array(size),
-    set_array: (array, index, value) => (array[index] = value),
-  },
-  load: {
-    load: instantiateBuffer,
-  },
-  string: {
-    to_int: () => {},
-    to_float: () => {},
-  },
-};
+    string: {
+      to_int: () => {},
+      to_float: () => {},
+    },
+  };
 
-if (override) {
-    Object.entries(importObject).forEach(([key, value]) => { importObject[key] = {...value, ...override[key]} });
-}
+  if (override) {
+    Object.entries(importObject).forEach(([key, value]) => {
+      importObject[key] = { ...value, ...override[key] };
+    });
+  }
 
-const instance = await wumbo();
+  const instance = await wumbo();
 
-const load_lua = instance.cwrap("load_lua", "number", ["array", "number"]);
-const clean_up = instance.cwrap("clean_up", "void", ["number"]);
-const get_size = instance.cwrap("get_size", "number", ["number"]);
-const get_data = instance.cwrap("get_data", "number", ["number"]);
+  const load_lua = instance.cwrap("load_lua", "number", ["array", "number"]);
+  const clean_up = instance.cwrap("clean_up", "void", ["number"]);
+  const get_size = instance.cwrap("get_size", "number", ["number"]);
+  const get_data = instance.cwrap("get_data", "number", ["number"]);
 
-return async (txt) => {
-  const bytes = new TextEncoder().encode(txt);
-  const result = load_lua(bytes, bytes.length);
-  const wasm_bytes = get_data(result);
-  const wasm_size = get_size(result);
+  return async (txt) => {
+    const bytes = new TextEncoder().encode(txt);
+    const result = load_lua(bytes, bytes.length);
+    const wasm_bytes = get_data(result);
+    const wasm_size = get_size(result);
 
-  const buffer = instance.HEAPU8.subarray(wasm_bytes, wasm_bytes + wasm_size);
-  const start = await instantiateBuffer(buffer);
-  clean_up(result);
-  return start;
-};
+    const buffer = instance.HEAPU8.subarray(wasm_bytes, wasm_bytes + wasm_size);
+    const start = await instantiateBuffer(buffer);
+    clean_up(result);
+    return start;
+  };
 };
